6718dffd650ee47db783b6f1b1149a5e7da0835f
Owen Dyckhoff <owen.dyckhoff@gmail.com>
2015-08-26 11:28:47 +0100

    Removed New-style comments in favour of old style.

--- strings/str.c
+++ strings/str.c
@@ -6,13 +6,13 @@
 #include "str.h"
 
 char *strrep(char *orig, char *rep, char *with) {
-    char *result; // the return string
-    char *ins;    // the next insert point
-    char *tmp;    // varies
-    int len_rep;  // length of rep
-    int len_with; // length of with
-    int len_front; // distance between rep and end of last rep
-    int count;    // number of replacements
+    char *result;  /* the return string */
+    char *ins;     /* the next insert point */
+    char *tmp;     /* varies */
+    int len_rep;   /* length of rep */
+    int len_with;  /* length of with */
+    int len_front; /* distance between rep and end of last rep */
+    int count;     /* number of replacements */
 
     if (!orig)
         return NULL;
@@ -28,11 +28,12 @@ char *strrep(char *orig, char *rep, char *with) {
         ins = tmp + len_rep;
     }
 
-    // first time through the loop, all the variable are set correctly
-    // from here on,
-    //    tmp points to the end of the result string
-    //    ins points to the next occurrence of rep in orig
-    //    orig points to the remainder of orig after "end of rep"
+    /* first time through the loop, all the variable are set correctly
+       from here on,
+          tmp points to the end of the result string
+          ins points to the next occurrence of rep in orig
+          orig points to the remainder of orig after "end of rep"
+    */
     tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
 
     if (!result)
@@ -43,7 +44,7 @@ char *strrep(char *orig, char *rep, char *with) {
         len_front = ins - orig;
         tmp = strncpy(tmp, orig, len_front) + len_front;
         tmp = strcpy(tmp, with) + len_with;
-        orig += len_front + len_rep; // move to next "end of rep"
+        orig += len_front + len_rep; /* move to next "end of rep" */
     }
     strcpy(tmp, orig);
 
